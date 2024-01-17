625179e935cbaad10680850490ceed54d8eaeb01
Fadin Artem <a.fadin-forwork@mail.ru>
2024-01-17 22:55:46 +0600

    add all styles

--- front/gta-6-front-end/src/home/Home.tsx
+++ front/gta-6-front-end/src/home/Home.tsx
@@ -1,14 +1,25 @@
+import { useForm } from 'react-hook-form'
 import styles from './Home.module.css'
+
+const isSuccess = false
 
 function Home() {
 
+  const {register} = useForm()
+
   return (
     <div className={styles.wrapper}>
-      <form>
-        <h1>GTA 6 - Оставь заявку</h1>
-        <input type="name" placeholder='Введите ваше имя: ' />
-        <input type="email" placeholder='Введите email:' />
-        <button>Хочу GTA!</button>
+        <form>
+      {isSuccess ? (
+        <div className={styles.success}>Форма отправлена</div>
+      ) : (
+        <>
+          <h1>GTA 6 -Оставь заявку</h1>
+          <input type="name" placeholder='Введите имя:' />
+          <input type="email" placeholder='Введите email:'/>
+          <button>Хочу GTA!</button>
+        </>
+      )}
       </form>
     </div>
   )
