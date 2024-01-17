9a07861ad8e168cd9d7ec86111d94eddb2ab4990
Fadin Artem <a.fadin-forwork@mail.ru>
2024-01-17 23:09:23 +0600

    add functions to form

--- front/gta-6-front-end/src/home/Home.tsx
+++ front/gta-6-front-end/src/home/Home.tsx
@@ -1,22 +1,33 @@
-import { useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import styles from './Home.module.css'
 
 const isSuccess = false
 
+interface IFormState {
+  name: string;
+  email: string;
+}
+
+const onSubmit: SubmitHandler<IFormState> = (data) => {
+  console.log(data)
+}
+
 function Home() {
 
-  const {register} = useForm()
+  const {register, handleSubmit} = useForm<IFormState>()
 
   return (
     <div className={styles.wrapper}>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
       {isSuccess ? (
         <div className={styles.success}>Форма отправлена</div>
       ) : (
         <>
           <h1>GTA 6 -Оставь заявку</h1>
-          <input type="name" placeholder='Введите имя:' />
-          <input type="email" placeholder='Введите email:'/>
+          <input type="name" placeholder='Введите имя:' {...
+            register('name')}/>
+          <input type="email" placeholder='Введите email:'{...
+            register('email')}/>
           <button>Хочу GTA!</button>
         </>
       )}
